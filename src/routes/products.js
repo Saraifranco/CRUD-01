c1497b96eb17e31135993c939476cc323f9f5139
Saraifranco <saraifranco@live.com.ar>
2022-02-25 20:25:03 -0300

    CRUD episodio II

---
+++ src/routes/products.js
@@ -0,0 +1,40 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination:(req, file, cb)=> {
+      cb(null, path.join(__dirname,"../public/images/descargas"))
+    },
+    filename:(req, file, cb)=> {
+        const newFile = date.now() + path.extname(file.originalname);
+     cb(null, newFile)
+    }
+  }) 
+  const upload = multer({ storage: storage })
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productsController');
+
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', productsController.index); 
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create', productsController.create); 
+router.post('/', upload.single('img'), productsController.store); 
+
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/detail/:id', productsController.detail); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', productsController.edit); 
+router.patch('/edit/:id', productsController.update); 
+
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/delete/:id', productsController.destroy); 
+
+
+module.exports = router;
